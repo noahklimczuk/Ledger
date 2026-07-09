f572a444a5c9951c12a2fc7c73ee578ff81f133f
Noah Klimczuk <noahklimczuk2001@gmail.com>
2026-07-09 12:28:37 -0400

    initial commit

---
+++ Ledger/Ledger/LedgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LedgerApp.swift
+//  Ledger
+//
+//  Created by Noah Klimczuk on 2026-07-09.
+//
+
+import SwiftUI
+
+@main
+struct LedgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
